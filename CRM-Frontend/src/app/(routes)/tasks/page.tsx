edaa4afe9b3dfc80d06c48ba203cf27132d4ab08
saadAlsaree <saad.alsaree@hotmail.com>
2024-09-01 12:02:10 +0300

    add loading

--- CRM-Frontend/src/app/(routes)/tasks/page.tsx
+++ CRM-Frontend/src/app/(routes)/tasks/page.tsx
@@ -1,28 +1,31 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 // import { getTasks } from '@/actions/projects/get-tasks';
 import { TasksDataTable } from './components/data-table';
 import { columns } from './components/columns';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { KanbanBoard } from './components/KanbanBoard';
+import SuspenseLoading from '@/components/loadings/suspense';
 
 const TasksPage = async () => {
    //  const tasks: any = await getTasks();
 
    return (
-      <div>
-         <Tabs defaultValue='tableTask'>
-            <TabsList>
-               <TabsTrigger value='tableTask'>مهامي</TabsTrigger>
-               <TabsTrigger value='kanban'>Kanban</TabsTrigger>
-            </TabsList>
-            <TabsContent value='tableTask'>
-               <TasksDataTable data={[]} columns={columns} />
-            </TabsContent>
-            <TabsContent value='kanban'>
-               <KanbanBoard />
-            </TabsContent>
-         </Tabs>
-         <div></div>
+      <div className='bg-white rounded-lg p-4 '>
+         <Suspense fallback={<SuspenseLoading />}>
+            <Tabs defaultValue='tableTask'>
+               <TabsList>
+                  <TabsTrigger value='tableTask'>مهامي</TabsTrigger>
+                  <TabsTrigger value='kanban'>Kanban</TabsTrigger>
+               </TabsList>
+               <TabsContent value='tableTask'>
+                  <TasksDataTable data={[]} columns={columns} />
+               </TabsContent>
+               <TabsContent value='kanban'>
+                  <KanbanBoard />
+               </TabsContent>
+            </Tabs>
+            <div></div>
+         </Suspense>
       </div>
    );
 };
--- CRM-Frontend/src/app/page.tsx
+++ CRM-Frontend/src/app/page.tsx
@@ -4,39 +4,43 @@ import ProjectCard from './_components/ProjectCard';
 import TaskCard from './_components/TaskCard';
 import EventCard from './_components/EventCard';
 import NotificationCard from './_components/NotificationCard';
+import { Suspense } from 'react';
+import SuspenseLoading from '@/components/loadings/suspense';
 
 export default async function Home() {
    return (
-      <ProtectedPage>
-         <div className='p-4 flex gap-4 flex-col md:flex-row'>
-            {/* lift */}
-            <div className='w-full lg:w-2/3 flex flex-col gap-6'>
-               {/* User Card */}
-               <div className='flex justify-between gap-4 flex-wrap'>
-                  <ProjectCard title='كل القضايا' description='320' />
+      <Suspense fallback={<SuspenseLoading />}>
+         <ProtectedPage>
+            <div className='p-4 flex gap-4 flex-col md:flex-row'>
+               {/* lift */}
+               <div className='w-full lg:w-2/3 flex flex-col gap-6'>
+                  {/* User Card */}
+                  <div className='flex justify-between gap-4 flex-wrap'>
+                     <ProjectCard title='كل القضايا' description='320' />
 
-                  <TaskCard title='المهام المفتوحة' description='6' />
-                  <EventCard title='الأحداث اليوم' description='0' />
-                  <NotificationCard title='الإشعارات' description='4' />
+                     <TaskCard title='المهام المفتوحة' description='6' />
+                     <EventCard title='الأحداث اليوم' description='0' />
+                     <NotificationCard title='الإشعارات' description='4' />
+                  </div>
+                  {/* Middle Charts */}
+                  <div className=' flex gap-4 flex-col lg:flex-row'>
+                     {/* Count Chart */}
+                     <div className='w-full lg:w-1/3 h-'>{/* <CountChart /> */}</div>
+
+                     {/* Attendance Chart */}
+                     <div className='w-full lg:w-2/3 h-'>{/* <AttendanceChart /> */}</div>
+                  </div>
+                  {/* Bottom Charts */}
+                  <div className='w-full h-[500px]'>{/* <FinanceChart /> */}</div>
                </div>
-               {/* Middle Charts */}
-               <div className=' flex gap-4 flex-col lg:flex-row'>
-                  {/* Count Chart */}
-                  <div className='w-full lg:w-1/3 h-'>{/* <CountChart /> */}</div>
 
-                  {/* Attendance Chart */}
-                  <div className='w-full lg:w-2/3 h-'>{/* <AttendanceChart /> */}</div>
-               </div>
-               {/* Bottom Charts */}
-               <div className='w-full h-[500px]'>{/* <FinanceChart /> */}</div>
-            </div>
-
-            {/* right */}
-            <div className='w-full lg:w-1/3 flex flex-col gap-8'>
-               {/* <EventCalender />
+               {/* right */}
+               <div className='w-full lg:w-1/3 flex flex-col gap-8'>
+                  {/* <EventCalender />
             <Announcements /> */}
+               </div>
             </div>
-         </div>
-      </ProtectedPage>
+         </ProtectedPage>
+      </Suspense>
    );
 }
